b818c4d408b32ed4692f6d49352b3f3462bd3698
jennisun <jsun20@stuy.edu>
2021-10-19 13:27:20 -0400

    added lines because lines are cool

--- list.h
+++ list.h
@@ -5,10 +5,17 @@ struct song_node{
 };
 
 struct song_node *insert_front(struct song_node *front, char* n, char* a);
+
 struct song_node *insert_order(struct song_node *front, char* n, char* a);
+
 void print_list(struct song_node* front);
+
 struct song_node *find_song(struct song_node *front, char* n, char* a);
+
 struct song_node *find_artist(struct song_node *front, char *n);
+
 struct song_node *get_random(struct song_node *front);
+
 struct song_node *remove_song(struct song_node *front, chat *n, char *a);
+
 struct song_node *free_list(struct song_node *front);
