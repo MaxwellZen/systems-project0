8ca8abe6d589336804d9f2bb1353ddda56301184
Maxwell Zen <maxwell.zen@gmail.com>
2021-10-22 10:51:05 -0400

    created structure of library.c file

---
+++ library.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "list.h"
+
+struct song_node **init_library (struct song_node **front) {
+
+}
+
+struct song_node **add_song (struct song_node **front, char *n, char *a) {
+
+}
+
+struct song_node *get_song (struct song_node **front, char *n, char *a) {
+
+}
+
+struct song_node *get_artist (struct song_node **front, char *a) {
+
+}
+
+void print_by_letter (struct song_node **front, char c) {
+
+}
+
+void print_by_artist (struct song_node **front, char *a) {
+
+}
+
+void print_library (struct song_node **front) {
+
+}
+
+void shuffle (struct song_node **front) {
+
+}
+
+struct song_node **delete_song (struct song_node **front, char *n, char *a) {
+
+}
+
+struct song_node **clear_library (struct song_node **front) {
+
+}
