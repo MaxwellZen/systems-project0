5f66af54a74563d5936adbf6cbd6e6ca9d5f21f5
Maxwell Zen <maxwell.zen@gmail.com>
2021-10-23 17:12:24 -0400

    altered parameters, wrote char_index method, drafted get_artist and print_by_letter

--- library.c
+++ library.c
@@ -5,33 +5,39 @@
 #include "list.h"
 
 struct song_node **init_library (struct song_node **front) {
-	front = malloc(27, sizeof(struct song_node *));
+	front = malloc(27 * sizeof(struct song_node *));
 	return front;
 }
 
-int artist_index(char *a) {
-	if (a[0]>='a' && a[0]<='z') return a[0]-'a';
-	if (a[0]>='A' && a[0]<='Z') return a[0]-'A';
+int char_index(char c) {
+	if (c>='a' && c<='z') return c-'a';
+	if (c>='A' && c<='Z') return c-'A';
 	return 26;
 }
 
-struct song_node **add_song (struct song_node **front, char *n, char *a) {
+int artist_index(char *a) {
+	return char_index(a[0]);
+}
+
+struct song_node **add_song (struct song_node **front, char *a, char *n) {
 	int i = artist_index(a);
-	front[i] = insert_order(front[i], n, a);
+	front[i] = insert_order(front[i], a, n);
 	return front;
 }
 
-struct song_node *get_song (struct song_node **front, char *n, char *a) {
+struct song_node *get_song (struct song_node **front, char *a, char *n) {
 	int i = artist_index(a);
-	return find_song(front[i], n, a);
+	return find_song(front[i], a, n);
 }
 
 struct song_node *get_artist (struct song_node **front, char *a) {
-
+	int i = artist_index(a);
+	return find_artist(front[i], a);
 }
 
 void print_by_letter (struct song_node **front, char c) {
-
+	int i = char_index(c);
+	print_list(front[i]);
 }
 
 void print_by_artist (struct song_node **front, char *a) {
@@ -46,7 +52,7 @@ void shuffle (struct song_node **front) {
 
 }
 
-struct song_node **delete_song (struct song_node **front, char *n, char *a) {
+struct song_node **delete_song (struct song_node **front, char *a, char *n) {
 
 }
 
